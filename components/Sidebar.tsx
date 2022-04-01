cd6eb3d024b5aa7658a3a7b861fe26a244037d04
hann <hanafichoirulloh1@gmail.com>
2022-04-01 22:23:56 +0700

    late

---
+++ components/Sidebar.tsx
@@ -0,0 +1,70 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRocketchat, faBitcoin } from '@fortawesome/free-brands-svg-icons'
+import { faSortAlphaDown, faEgg } from '@fortawesome/free-solid-svg-icons'
+
+function Sidebar() {
+  return (
+    <header className="z-10 grid h-16 grid-cols-4 items-center bg-[#C1292E] lg:absolute lg:right-0 lg:top-0 lg:flex lg:h-full lg:w-16 lg:flex-col lg:justify-between">
+      {/*  */}
+      <div className="col-span-1 grid text-[#161925]">
+        <FontAwesomeIcon
+          icon={faBitcoin}
+          color="#ffff"
+          className="m-auto w-10 lg:mt-5 "
+        />
+      </div>
+      {/* Social */}
+      <div className="col-span-2 h-16 text-[#161925] lg:flex lg:h-24 lg:w-16 lg:flex-col">
+        <div className="grid grid-cols-2 lg:flex lg:flex-col">
+          {/* 1 */}
+          <Link href="/">
+            <div className="group col-span-1 h-16 cursor-pointer items-center border-r-2 border-l-2 px-2 transition delay-75 ease-in-out hover:p-0 lg:flex lg:h-12 lg:w-16 lg:flex-col lg:border-none">
+              <FontAwesomeIcon
+                icon={faSortAlphaDown}
+                color="#ffff"
+                className="m-auto w-9 py-3 group-hover:hidden lg:py-0 lg:group-hover:inline-block"
+              />
+              <h1 className="mt-3 hidden w-full text-center text-3xl group-hover:inline-block lg:absolute lg:bg-[#161925] lg:text-[12px] lg:text-[#FDFFFC] lg:group-hover:-translate-x-16 lg:group-hover:-translate-y-1">
+                home
+              </h1>
+            </div>
+          </Link>
+
+          {/* 2 */}
+          <Link href="/about">
+            <div className="group col-span-1 h-16 cursor-pointer items-center border-r-2  px-2 transition delay-75 ease-in-out hover:p-0 lg:flex lg:h-12 lg:w-16 lg:flex-col lg:border-none">
+              <FontAwesomeIcon
+                icon={faEgg}
+                color="#ffff"
+                className="m-auto w-7 py-3 group-hover:hidden lg:py-0 lg:group-hover:inline-block "
+              />
+              <h1 className="mt-3 hidden w-full text-center text-3xl group-hover:inline-block lg:absolute lg:bg-[#161925] lg:text-[12px] lg:text-[#FDFFFC] lg:group-hover:-translate-x-16 lg:group-hover:-translate-y-1">
+                about
+              </h1>
+            </div>
+          </Link>
+        </div>
+      </div>
+
+      {/* chat */}
+      <div className="col-span-1">
+        <a
+          href="https://moma-web3.vercel.app/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <FontAwesomeIcon
+            icon={faRocketchat}
+            color="#ffff"
+            className="m-auto w-10 lg:mb-5"
+          />
+        </a>
+      </div>
+    </header>
+  )
+}
+
+export default Sidebar
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,46 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
+import Sidebar from '../components/Sidebar'
 import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="overflow-hidden bg-[#FDFFFC]">
       <Head>
-        <title>Create Next App</title>
+        <title>NFT • Drop</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div>
+        <Sidebar />
+        <div className=" m-10 mx-auto flex max-w-lg flex-col-reverse lg:m-0 lg:grid lg:h-screen lg:max-w-none lg:grid-cols-10">
+          <div className="col-span-4 p-10">
+            <div className="flex flex-col justify-center lg:min-h-[750px]">
+              <h1 className="mt-10 text-3xl font-bold">NFT • Drop</h1>
+              <h2>
+                brings artists and creators together on a single platform.
+              </h2>
+              <div className="cursor-pointer py-2">
+                <Link href="/nft/mint">
+                  <div className="w-14 border-2 border-[#C1292E] bg-[#FDFFFC] text-center hover:bg-[#C1292E] hover:text-[#FDFFFC]">
+                    MINT
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-6 items-center justify-center p-9">
+            <div className="flex flex-col justify-center lg:min-h-screen">
+              <Image
+                src="https://i.postimg.cc/25SCWb0t/Desain-tanpa-judul.png"
+                height="100px"
+                width="100px"
+                layout="responsive"
+              />
+            </div>
+          </div>
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+      </div>
     </div>
   )
 }
